63e247d86f753e86ef818fceae07069d3452f9e0
jdcorona96 <josed.hc96@gmail.com>
2019-10-14 20:22:56 -0700

    set file image setup

--- src/main/java/view/CreateRecipeView.java
+++ src/main/java/view/CreateRecipeView.java
@@ -1,5 +1,9 @@
 package view;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -7,17 +11,21 @@ import javafx.scene.control.ListView;
 //import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.stage.FileChooser;
 //import model.Ingredient;
 
 public class CreateRecipeView extends GridPane {
 
+	private final String url = "src/main/resources/images/preview.png";
+	
 	private Label recipeName;
 	private TextField recipeNameField;
 	private Label recipeSummary;
 	private TextField recipeSummaryField;
-
 	private TextField ingredientField;
 	private HBox ingredientButtons;
 	private Button addIngredientButton;
@@ -28,19 +36,30 @@ public class CreateRecipeView extends GridPane {
 	private Label instructions;
 	private TextArea instructionsField;
 	private Button submitButton;
+	private ImageView image;
+	private Button chooseFile;
+	private FileChooser fileChooser;
 
 	public CreateRecipeView() {
 
-		initializeAllNodes();
+		try {
+			initializeAllNodes();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		setNodesToParent();
 		setIngredientButtonsHandler();
+		setChooseFileButton();
 	}
 
-	private void initializeAllNodes() {
+	private void initializeAllNodes() throws FileNotFoundException {
 
 		recipeName = new Label("Recipe Name");
 		recipeNameField = new TextField();
 		recipeSummary = new Label("Summary");
+		image = new ImageView();
+		image.setImage(new Image(new FileInputStream(url)));
 		recipeSummaryField = new TextField();
 		ingredientField = new TextField("1 tomatoe");
 		ingredientButtons = new HBox(5);
@@ -54,6 +73,8 @@ public class CreateRecipeView extends GridPane {
 		instructions = new Label("Instructions");
 		instructionsField = new TextArea();
 		submitButton = new Button("Submit");
+		chooseFile = new Button("choose file");
+		fileChooser = new FileChooser();
 	}
 
 	private void setNodesToParent() {
@@ -63,11 +84,13 @@ public class CreateRecipeView extends GridPane {
 		this.setVgap(gap);
 		this.add(recipeName, 0, 0);
 		this.add(recipeNameField, 1, 0);
+		this.add(image,2,0,1,3);
 		this.add(recipeSummary, 0, 1);
 		this.add(recipeSummaryField, 1, 1);
 		this.add(ingredientsList, 0, 2, 2, 2);
 		this.add(ingredientField, 1, 4);
 		this.add(ingredientButtons, 0, 4);
+		this.add(chooseFile,2,4);
 		this.add(instructions, 0, 5);
 		this.add(instructionsField, 1, 5);
 		this.add(submitButton, 0, 6, 2, 1);
@@ -106,5 +129,16 @@ public class CreateRecipeView extends GridPane {
 		});
 	}
 	
+	private void setChooseFileButton() {
+		chooseFile.setOnAction(ae -> {
+			
+			fileChooser.setTitle("Choose Image");
+			File file = fileChooser.showOpenDialog(this.getScene().getWindow());
+			
+			image.setImage(new Image(file.toURI().toString()));
+			
+			
+		});
+	}
 	
 }
